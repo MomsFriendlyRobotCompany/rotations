97493450b32c7f27a747757fb6c7ada299331af7
walchko <walchko@users.noreply.github.com>
2020-05-27 21:56:15 -0400

    Update rotations.py

--- rotations/rotations.py
+++ rotations/rotations.py
@@ -72,3 +72,43 @@ def R313(a,b,c, degrees=False):
             [s2*s3, c3*s2, c2]
         ]
     )
+
+
+def R312(a,b,c, degrees=False):
+    """Returns a rotation matrix based on: Z1*X2*Z3"""
+    if degrees:
+        a *= deg2rad
+        b *= deg2rad
+        c *= deg2rad
+
+    s3 = np.sin(c); c3 = np.cos(c)
+    s2 = np.sin(b); c2 = np.cos(b)
+    s1 = np.sin(a); c1 = np.cos(a)
+
+    return np.array(
+        [
+            [c1*c3-s1*s2*s3, -c2*s1, c1*s3+c3*s1*s2],
+            [c3*s1+c1*s2*s3,  c1*c2, s1*s3-c1*c3*s2],
+            [        -c2*s3,     s2,          c2*c3]
+        ]
+    )
+
+
+def R321(a,b,c, degrees=False):
+    """Returns a rotation matrix based on: Z1*X2*Z3"""
+    if degrees:
+        a *= deg2rad
+        b *= deg2rad
+        c *= deg2rad
+
+    s3 = np.sin(c); c3 = np.cos(c)
+    s2 = np.sin(b); c2 = np.cos(b)
+    s1 = np.sin(a); c1 = np.cos(a)
+
+    return np.array(
+        [
+            [c1*c2, c1*s2*s3-c3*s1, s1*s3+c1*c3*s2],
+            [c2*s1, c1*c3+s1*s2*s3, c3*s1*s2-c1*s3],
+            [  -s2,          c2*s3,          c2*c3]
+        ]
+    )

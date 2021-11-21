44a208fda0a40d494aeeeaf6a48b1ee0d210fa1e
walchko <walchko@users.noreply.github.com>
2021-11-20 18:38:57 -0700

    update python

--- rotations/__init__.py
+++ rotations/__init__.py
@@ -4,10 +4,10 @@
 # Copyright (c) 2020 Kevin Walchko
 # see LICENSE for full details
 ##############################################
-try:
-    from importlib.metadata import version # type: ignore
-except ImportError:
-    from importlib_metadata import version # type: ignore
+# try:
+from importlib.metadata import version # type: ignore
+# except ImportError:
+#     from importlib_metadata import version # type: ignore
 
 from rotations.rotations import R1,R2,R3,R321,R123,R313
 
